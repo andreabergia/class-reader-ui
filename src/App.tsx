16fc1e91c01f38c7e2c16b28c38a932d39d8185d
Andrea Bergia <andreabergia@gmail.com>
2023-09-09 16:31:56 +0200

    feat: file selector

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import "./App.css";
 import { Header } from "./Header";
+import { ClassFileSelector } from "./ClassFileSelector";
 
 export function App() {
+  function onClassFileSelected(classFile: File) {
+    console.log("selected class file", classFile);
+  }
+
   return (
     <>
       <Header />
+      <main>
+        <ClassFileSelector onClassFileSelected={onClassFileSelected} />
+      </main>
     </>
   );
 }
---
+++ src/ClassFileSelector.tsx
@@ -0,0 +1,40 @@
+import { useCallback } from "react";
+import { useDropzone } from "react-dropzone";
+import "./ClassFileSelector.css";
+
+interface ClassFileSelectorProps {
+  onClassFileSelected: (classFile: File) => void;
+}
+
+export function ClassFileSelector({
+  onClassFileSelected,
+}: ClassFileSelectorProps) {
+  const onDrop = useCallback(
+    (acceptedFiles: File[]) => {
+      console.debug("dropped files", acceptedFiles);
+      if (acceptedFiles && acceptedFiles.length > 0) {
+        onClassFileSelected(acceptedFiles[0]);
+      }
+    },
+    [onClassFileSelected]
+  );
+
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({
+    onDrop,
+    accept: { "application/java-vm": [".class"] },
+    multiple: false,
+  });
+
+  return (
+    <div {...getRootProps()} className="class-file-selector">
+      <input {...getInputProps()} />
+      {isDragActive ? (
+        <p>Drop the file here…</p>
+      ) : (
+        <p>
+          Drag and drop or click to select a <code>.class</code> file
+        </p>
+      )}
+    </div>
+  );
+}
